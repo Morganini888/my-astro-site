dbe2fe1d8074fd26e7967515335acd43a0e9a610
Morganini888 <33400702+Morganini888@users.noreply.github.com>
2022-09-17 15:56:41 -0700

    .css .ts

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Goodlander Experience Training (GET)';
+export const SITE_DESCRIPTION = 'Altering Our Experience of the World';
